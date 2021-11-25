e7f5660075704f2e18154673dff130b1e4dc6b0c
mbpitts <mbpitts@go.olemiss.edu>
2021-11-25 05:09:18 -0600

    nevermind, about to use components to keep this as single page app

--- public/scripts/index-public.js
+++ public/scripts/index-public.js
@@ -1,8 +1,17 @@
+// --- index variables ---------------------------------------------------------------------------------------------------
 var PER_ASSESSMENT = document.getElementById('per');
 var COG_ASSESSMENT = document.getElementById('cog');
 var USER_INFO_FIELD = document.getElementById('visitor-info');
 var USER_INFO_FIELD_2 = document.getElementById('visitor-info-2');
 var LANDING_TEXT = document.getElementById('landing-text');
+
+// --- admin terminal variables ---------------------------------------------------------------------------------------------------
+var USER_RESULTS = document.getElementById('main-user-results');
+var ANALYTICS = document.getElementById('main-analytics');
+var ACCT_MGMT = document.getElementById('main-acct-mgmt');
+var ADVANCED = document.getElementById('main-advanced');
+
+// --- index js ---------------------------------------------------------------------------------------------------
 
 // for 'continue' button on personality assessment. 
 // hides personality assessment and shows cognitive assessment.
@@ -51,7 +60,48 @@ function toggle_visibility_5(){
     console.log("clicked 'begin assessment. hide landing page paragraph and show personality assessment")
 }
 
+// if requirements for submission are met, assessment is submitted and answers, visitor name, and visitor email are stored in the database.
+// confirmation email is sent and confirmation message is displayed.
+// if requirements are not met, displays error message.
 function submit_assessment() {
     'use strict';
     console.log("clicked 'submit assessment'. If assessment is properly completed, show thank you message and send confirmation email. If not properly completed, show error message.")
+}
+
+// --- admin terminal js ---------------------------------------------------------------------------------------------------
+
+// shows user results tab in adminal terminal main
+function show_user_results(){
+    USER_RESULTS.style.display = 'flex';
+    ANALYTICS.style.display = 'none';
+    ACCT_MGMT.style.display = 'none';
+    ADVANCED.style.display = 'none';
+    console.log('show user results in admin terminal main');
+}
+
+// shows analytics tab in adminal terminal main
+function show_analytics(){
+    USER_RESULTS.style.display = 'none';
+    ANALYTICS.style.display = 'flex';
+    ACCT_MGMT.style.display = 'none';
+    ADVANCED.style.display = 'none';
+    console.log('show analytics in admin terminal main');
+}
+
+// shows account management tab in adminal terminal main
+function show_acct_mgmt(){
+    USER_RESULTS.style.display = 'none';
+    ANALYTICS.style.display = 'none';
+    ACCT_MGMT.style.display = 'flex';
+    ADVANCED.style.display = 'none';
+    console.log('show account managment in admin terminal main');
+}
+
+// shows advanced tab in adminal terminal main
+function show_advanced(){
+    USER_RESULTS.style.display = 'none';
+    ANALYTICS.style.display = 'none';
+    ACCT_MGMT.style.display = 'none';
+    ADVANCED.style.display = 'flex';
+    console.log('show advanced in admin terminal main');
 }
