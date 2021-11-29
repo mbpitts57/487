9b4d13e158cdae457d1a033f4f857bb10db09f1e
mbpitts <mbpitts@go.olemiss.edu>
2021-11-29 01:38:10 -0600

    admin log in looks better

--- src/components/AdminTerminal2.js
+++ src/components/AdminTerminal2.js
@@ -4,7 +4,7 @@ import reportWebVitals from "../reportWebVitals";
 import React, { useState, useEffect } from "react";
 import "../styles/App.css";
 import { API } from "aws-amplify";
-import { withAuthenticator, AmplifySignOut, AmplifyAuthenticator, AmplifySignUp } from '@aws-amplify/ui-react';
+import { withAuthenticator, AmplifySignOut, AmplifyAuthenticator, AmplifyAuthContainer, AmplifySignUp } from '@aws-amplify/ui-react';
 import { listVisitors } from "../graphql/queries";
 import { deleteVisitor as deleteVisitorMutation } from "../graphql/mutations";
 // import { Visitor } from '../models';
@@ -101,85 +101,89 @@ export function AdminTerminal2() {
   }
 
   return authState === AuthState.SignedIn && user ? (
-    <AmplifyAuthenticator>
-      <AmplifySignOut />
-      <div className="AdminTerminal2">
-        <div className="admin-terminal-con">
-          <span className="admin-terminal-title">Administrative Portal</span>
-          <div className="admin-terminal-content">
-            <nav className="admin-terminal-sidebar">
-              <button type="button" onclick={show_user_results}> user results </button>
-              <button type="button" onclick={show_analytics}> analytics </button>
-              <button type="button" onclick={show_acct_mgmt}> account management </button>
-              <button type="button" onclick={show_advanced}> advanced </button>
-            </nav>
-            <div className="admin-terminal-main">
-              <div className="main-user-results" id="main-user-results">
+    <AmplifyAuthContainer>
+      <AmplifyAuthenticator>
+        <AmplifySignOut />
+        <div className="AdminTerminal2">
+          <div className="admin-terminal-container">
+            <span className="admin-terminal-title">Administrative Portal</span>
+            <div className="admin-terminal-content">
+              <nav className="admin-terminal-sidebar">
+                <button type="button" onclick={show_user_results}> user results </button>
+                <button type="button" onclick={show_analytics}> analytics </button>
+                <button type="button" onclick={show_acct_mgmt}> account management </button>
+                <button type="button" onclick={show_advanced}> advanced </button>
+              </nav>
+              <div className="admin-terminal-main">
+                <div className="main-user-results" id="main-user-results">
 
-                <h1 className="admin-terminal-h1">Review User Entries</h1>
-                {/* shows a list of all user entries */}
-                <div className="fetchingVisitors">
-                  <div className="main-content mc-fetch-visitors">
-                    {Visitors.map((Visitor) => (
-                      <div className="row" key={Visitor.id || Visitor.visName}>
-                        <p>{Visitor.visName}&nbsp;</p>
-                        <p>{Visitor.visEmail}&nbsp;</p>
+                  <h1 className="admin-terminal-h1">Review User Entries</h1>
+                  {/* shows a list of all user entries */}
+                  <div className="fetchingVisitors">
+                    <div className="main-content mc-fetch-visitors">
+                      {Visitors.map((Visitor) => (
+                        <div className="row" key={Visitor.id || Visitor.visName}>
+                          <p>{Visitor.visName}&nbsp;</p>
+                          <p>{Visitor.visEmail}&nbsp;</p>
 
-                        {/* just changes this from visitor to visitor.id might not work */}
-                        <button className="delete-btn" onClick={() => deleteVisitor(Visitor)}>
-                          Delete Visitor
-                        </button>
-                      </div>))
-                    }
-                    <button onClick={listEntries}>
-                      Console Log all Visitors
-                    </button>
-                  </div>
+                          {/* just changes this from visitor to visitor.id might not work */}
+                          <button className="delete-btn" onClick={() => deleteVisitor(Visitor)}>
+                            Delete Visitor
+                          </button>
+                        </div>))
+                      }
+                      <button onClick={listEntries}>
+                        Console Log all Visitors
+                      </button>
+                    </div>
 
-                  <div className="search-bar">((search icon)) Search: </div>
-                  <div className="filter-users">((button, dropdown menu)Filter Results</div>
-                  <div>
-                    [search bar, filter by scores, name alphabetical, most recent, oldest
-                    rows of all user inputs fetched from database]
+                    <div className="search-bar">((search icon)) Search: </div>
+                    <div className="filter-users">((button, dropdown menu)Filter Results</div>
+                    <div>
+                      [search bar, filter by scores, name alphabetical, most recent, oldest
+                      rows of all user inputs fetched from database]
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-            <div className="main-analytics" id="main-analytics">
-              <div className="main-content">
-                [visualisations/graphs, button where data can be downloaded as a csv
-                file if clicked]
+              <div className="main-analytics" id="main-analytics">
+                <div className="main-content">
+                  [visualisations/graphs, button where data can be downloaded as a csv
+                  file if clicked]
+                </div>
               </div>
-            </div>
-            <div className="main-acct-mgmt" id="main-acct-mgmt">
-              <div className="main-content">
-                <div id="change-password"></div>
-                [request password change link to be sent to email]
+              <div className="main-acct-mgmt" id="main-acct-mgmt">
+                <div className="main-content">
+                  <div id="change-password"></div>
+                  [request password change link to be sent to email]
+                </div>
               </div>
-            </div>
-            <div className="main-advanced" id="main-advanced">
-              <div className="main-content">
-                <a href="https://us-east-2.console.aws.amazon.com/amplify/home?region=us-east-2#/dvrouwci8gops">Navigate
-                  to the AWS Amplify Console</a>
-                [tools for advanced admins. can create new accounts, can send reset
-                password links to admin emails can view list of all admin accounts]
+              <div className="main-advanced" id="main-advanced">
+                <div className="main-content">
+                  <a href="https://us-east-2.console.aws.amazon.com/amplify/home?region=us-east-2#/dvrouwci8gops">Navigate
+                    to the AWS Amplify Console</a>
+                  [tools for advanced admins. can create new accounts, can send reset
+                  password links to admin emails can view list of all admin accounts]
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-    </AmplifyAuthenticator>
+      </AmplifyAuthenticator>
+    </AmplifyAuthContainer>
   ) : (
-    <AmplifyAuthenticator>
-      <AmplifySignUp
-        slot="sign-up"
-        formFields={[
-          { type: "username" },
-          { type: "password" },
-          { type: "email" }
-        ]}
-      />
-    </AmplifyAuthenticator>
+    <AmplifyAuthContainer>
+      <AmplifyAuthenticator>
+        <AmplifySignUp
+          slot="sign-up"
+          formFields={[
+            { type: "username" },
+            { type: "password" },
+            { type: "email" }
+          ]}
+        />
+      </AmplifyAuthenticator>
+    </AmplifyAuthContainer >
   );
 }
 
